f0f53e9d1dd91819366627a8fb96e5b69c3639ac
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-02-21 14:30:19 -0600

    Got the functiality of the website working, now just need to update the routing and add the search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
+import {
+  Route,
+  Switch
+} from "react-router-dom";
+
 
 export default function App() {
+
   return (
     <main>
+      <Switch>
+      
+      <Route path="/characters/">
+      <CharacterList/>
+      </Route>
+      <Route  path="/">
       <Header />
+      
       <WelcomePage/>
-    </main>
+      </Route>
+      </Switch>
+      </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,29 @@
 import React from "react";
+import {
+  Card,
+  Button,
+  CardHeader,
+  CardFooter,
+  CardBody,
+  CardTitle,
+  CardText,
+  Col
+} from "reactstrap";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+export default function CharacterCard(props) {
+  console.log(props.data);
+  return (
+    
+    <Col xs="6" md="4" xl="3">
+    <Card>
+    <img src={props.data.image} />
+    <CardHeader>Name: {props.data.name}</CardHeader>
+    <CardText>Species: {props.data.species}</CardText>
+    <CardText>Gender: {props.data.gender}</CardText>
+    <CardText>Location: {props.data.location.name}</CardText>
+    <CardText>Status: {props.data.status}</CardText>
+    </Card>
+    </Col>
+  )
 }
